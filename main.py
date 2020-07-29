fb15f17421be6c878db53dfbe490d38ddab6488c
Hyungjun Yoo <yoohjun@gmail.com>
2020-07-29 13:39:37 +0900

    add : fake_infect_full, real_data_per_half_hour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from models import GC_TG_LSTM
 from data import data_loader
-# from data_new_idx import data_loader_new_idx
+from data_new_idx import data_loader_new_idx
+from data_half import data_loader_half
 
 import argparse
 import os
@@ -36,7 +37,9 @@ def parse_args():
     parser.add_argument('--all_concat', help='temp_all + lstm_inputs + lstm_output concat fc layer. without it, just lstm output = output', action='store_true')
     
     # Miscellaneous
+    parser.add_argument('--task', default='real_feb', type=str, help='which task to train, real_feb or fake_full')
     parser.add_argument('--data_path', default='/home/hyungjun/jupyter/KT_covid19/data/', type=str, help='base path for preprocessed dataset')
+    parser.add_argument('--data_normalize', help='+ lstm_output concat fc layer. without it, just lstm output = output', action='store_true')
     parser.add_argument('--result_path', default='./results/', type=str, help='directory for results')
 #     parser.add_argument('--save_steps', default=100, type=int, help='model and data save steps')
 #     parser.add_argument('--seed', default=2020, type=int, help='random seed')
@@ -44,16 +47,16 @@ def parse_args():
     args = parser.parse_args()
 
     return args
+    
 
-
-def main(model, train_dataloader, test_dataloader, learning_rate, num_epochs, args):
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, betas=0.9)
+def main(model, train_dataloader, test_dataloader, min_max_values, learning_rate, num_epochs, args):
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=len(train_dataloader)*args.num_epochs/10)
     
     model.to(args.device)
     model.train()
     
-    train_log_pd = pd.DataFrame(np.zeros([len(train_dataloader)*num_epochs, 2]), columns=['iterations', 'train_loss'])
+    train_log_pd = pd.DataFrame(np.zeros([len(train_dataloader)*num_epochs, 3]), columns=['iterations', 'train_user_loss', 'train_infect_loss'])
     valid_log_pd = pd.DataFrame(np.zeros([len(test_dataloader)*num_epochs, 3]), columns=['iterations', 'mse_valid_loss', 'mae_valid_loss'])
     min_valid_log = 1e+2
     
@@ -64,15 +67,18 @@ def main(model, train_dataloader, test_dataloader, learning_rate, num_epochs, ar
                 #print('train_input : ',train_input)
                 train_target = data[1].type(torch.FloatTensor).to(args.device)
                 #print('train_target : ',train_target)
-
                 # train
-                output = model(train_input)
-                loss = torch.nn.functional.mse_loss(output, train_target.squeeze(0), )
+                user_output, infect_output = model(train_input, min_max_values, args.task)
+                user_loss = torch.nn.functional.mse_loss(user_output, train_target[0].squeeze(0))
+                infect_loss = torch.nn.functional.mse_loss(infect_output, train_target[1].squeeze(0))
+                loss = user_loss + infect_loss
                 
                 train_log_pd['iterations'][epoch*len(train_dataloader)+i] = epoch*len(train_dataloader)+i
-                train_log_pd['train_loss'][epoch*len(train_dataloader)+i] = loss.item()
+                train_log_pd['train_user_loss'][epoch*len(train_dataloader)+i] = user_loss.item()
+                train_log_pd['train_infect_loss'][epoch*len(train_dataloader)+i] = infect_loss.item()
 
-                filename = os.path.join('lr{}_d{}-{}-{}_s{}_cat{}_temp{}_all{}_lstm-h-dim{}_lstm-in-dim{}_g-user-dim{}_g-infect-dim{}'.format(
+                filename = os.path.join('{}_lr{}_d{}-{}-{}_s{}_cat{}_temp{}_all{}_lstm-h-dim{}_lstm-in-dim{}_g-user-dim{}_g-infect-dim{}'.format(
+                                                                                                                    args.task,
                                                                                                                     args.learning_rate, 
                                                                                                                     args.train_days,
                                                                                                                     args.delay_days, 
@@ -97,16 +103,16 @@ def main(model, train_dataloader, test_dataloader, learning_rate, num_epochs, ar
             scheduler.step()
                 
             # validation
-            mean_mse_valid_log, mean_mae_valid_log = validation(model, test_dataloader, args.device)
+            mean_valid_user_log, mean_valid_infect_log = validation(model, test_dataloader, args.device)
             valid_log_pd['iterations'][epoch*len(test_dataloader)+i] = epoch*len(test_dataloader)+i
-            valid_log_pd['mse_valid_loss'][epoch*len(test_dataloader)+i] = mean_mse_valid_log
-            valid_log_pd['mae_valid_loss'][epoch*len(test_dataloader)+i] = mean_mae_valid_log
+            valid_log_pd['valid_user_loss'][epoch*len(test_dataloader)+i] = mean_valid_user_log
+            valid_log_pd['valid_infectloss'][epoch*len(test_dataloader)+i] = mean_valid_infect_log
             valid_log_pd.to_csv(args.result_path + 'valid_' + filename + '.csv', index=False)
+            mean_mse_valid_log = mean_valid_user_log + mean_valid_infect_log
             if min_valid_log >= mean_mse_valid_log:
                 min_valid_log = mean_mse_valid_log
                 print('best valid update => epochs : {}, iter : {}, loss : {}'.format(epoch, i, min_valid_log))
                 best_valid_model = model
-
                 model_path = os.path.join(args.result_path+'models/' + filename)
                 if not os.path.exists(model_path):
                     os.makedirs(model_path, exist_ok=True)
@@ -115,9 +121,7 @@ def main(model, train_dataloader, test_dataloader, learning_rate, num_epochs, ar
                     state_dict = model.state_dict()
                     torch.save(state_dict, f)
 
-            
-
-def validation(model, dataloader, device):
+def validation(model, dataloader, min_max_values, task, device):
     model.eval()
     
     mse_valid_log, mae_valid_log = [], []
@@ -126,22 +130,24 @@ def validation(model, dataloader, device):
             test_input = data[0]
             test_target = data[1].type(torch.FloatTensor).to(device)
             
-            output = model(test_input)
-            mse_loss = torch.nn.functional.mse_loss(output, test_target.squeeze(0))
-            mae_loss = torch.nn.functional.l1_loss(output, test_target.squeeze(0))
-            mse_valid_log.append(mse_loss.item())
-            mae_valid_log.append(mae_loss.item())
+            user_output, infect_output = model(test_input, min_max_values, task)
+            print('output : ', output)
+            print('test   : ', test_target.squeeze(0))
+            user_loss = torch.nn.functional.mse_loss(user_output, test_target[0].squeeze(0))
+            infect_loss = torch.nn.functional.mse_loss(infect_output, test_target[1].squeeze(0))
+            valid_user_log.append(user_loss.item())
+            valid_infect_log.append(infect_loss.item())
             
     model.train()
     
-    return np.mean(mse_valid_log), np.mean(mae_valid_log)
+    return np.mean(valid_user_log), np.mean(valid_infect_log)
         
 if __name__ == '__main__':
     args = parse_args()
 #     make_result_dir(args)
-    train_dataset, train_loader, test_dataset, test_loader = data_loader(args)
+    train_dataset, train_loader, test_dataset, test_loader, min_max_values = data_loader_half(args)
     model = GC_TG_LSTM(args)
 
     print('=== training start ===' )
-    main(model, train_loader, test_loader, learning_rate=args.learning_rate, num_epochs=args.num_epochs, args=args)
+    main(model, train_loader, test_loader, min_max_values, learning_rate=args.learning_rate, num_epochs=args.num_epochs, args=args)
     
--- models.py
+++ models.py
@@ -8,6 +8,9 @@ import networkx as nx
 from torch_geometric.nn import GCNConv
 #vfrom torch_geometric.nn import
 
+def denormalize(x, min_val, max_val):
+    return x * (max_val - min_val) + min_val
+
 class GC_TG_LSTM(nn.Module):
     def __init__(self, args):
         super(GC_TG_LSTM, self).__init__()
@@ -18,6 +21,7 @@ class GC_TG_LSTM(nn.Module):
         self.lstm_input_size = args.lstm_input_size
         self.lstm_hidden_size = args.lstm_hidden_size
         self.lstm_num_layers = args.lstm_num_layers
+        self.num_dong = args.num_dong
         # architecture switch
         self.temp_all_concat = args.temp_all_concat
         self.concat_fc = args.concat_fc
@@ -29,7 +33,7 @@ class GC_TG_LSTM(nn.Module):
         self.graph_conv_user2 =  GCNConv(args.graph_conv_feature_dim_user, args.graph_conv_feature_dim_user)
         self.graph_act1_user2 =  nn.ReLU() # nn.LeakyReLU(negative_slope=0.1) #
         
-        self.graph_fc_user = nn.Linear(142 * args.graph_conv_feature_dim_user, args.lstm_input_size)
+        self.graph_fc_user = nn.Linear(self.num_dong * args.graph_conv_feature_dim_user, args.lstm_input_size)
         self.graph_act2_user = nn.ReLU() # nn.LeakyReLU(negative_slope=0.1) #
         # self.graph_fc_user2 = nn.Linear()
         # sefl.graph_act2_user2 = nn.ReLU()
@@ -39,12 +43,12 @@ class GC_TG_LSTM(nn.Module):
         self.graph_act1_infect1 =  nn.ReLU()  # nn.LeakyReLU(negative_slope=0.1) #
         self.graph_conv_infect2 = GCNConv(args.graph_conv_feature_dim_infect, args.graph_conv_feature_dim_infect)
         self.graph_act1_infect2 =  nn.ReLU()  # nn.LeakyReLU(negative_slope=0.1) #
-        self.graph_fc_infect = nn.Linear(142 * args.graph_conv_feature_dim_infect, args.lstm_input_size)
+        self.graph_fc_infect = nn.Linear(self.num_dong * args.graph_conv_feature_dim_infect, args.lstm_input_size)
         self.graph_act2_infect = nn.ReLU() #  nn.LeakyReLU(negative_slope=0.1) #
         
         # test fc layer
-        self.test_fc_user = nn.Linear(142, args.lstm_input_size)
-        self.test_fc_infect = nn.Linear(142, args.lstm_input_size)
+        self.test_fc_user = nn.Linear(self.num_dong, args.lstm_input_size)
+        self.test_fc_infect = nn.Linear(self.num_dong, args.lstm_input_size)
         
         # concatenate FC
         self.concat_fc = nn.Linear(args.lstm_input_size*3, args.lstm_input_size)
@@ -54,23 +58,23 @@ class GC_TG_LSTM(nn.Module):
         
         self.lstm = nn.LSTM(input_size=args.lstm_input_size, hidden_size=args.lstm_hidden_size, num_layers=args.lstm_num_layers)
         self.lstm_act1 =  nn.ReLU() # nn.LeakyReLU(negative_slope=0.1) #
-        self.lstm_fc = nn.Linear(args.lstm_hidden_size, args.num_dong)
+        self.lstm_fc = nn.Linear(args.lstm_hidden_size, self.num_dong*2)
         self.lstm_act2 = nn.ReLU() # nn.LeakyReLU(negative_slope=0.1) #
         
         # temp signal single day 
-        self.temp_single_fc = nn.Linear(33, args.lstm_input_size)
+        self.temp_single_fc = nn.Linear(57, args.lstm_input_size)
         self.temp_single_act = nn.ReLU() # nn.LeakyReLU(negative_slope=0.1)  # 
         
         # temp signal all
-        self.temp_all_fc = nn.Linear(33*24*self.lstm_sequence_length, args.num_dong)
+        self.temp_all_fc = nn.Linear(57*48*self.lstm_sequence_length, self.num_dong*2)
         self.temp_all_act =  nn.ReLU() # nn.LeakyReLU(negative_slope=0.1) #
-        self.temp_all_output_fc = nn.Linear(args.num_dong*2, args.num_dong)
+        self.temp_all_output_fc = nn.Linear(args.num_dong*2, args.num_dong*2)
 
-        self.all_cat_fc = nn.Linear(24*self.lstm_sequence_length*args.lstm_input_size, args.num_dong)
+        self.all_cat_fc = nn.Linear(48*self.lstm_sequence_length*args.lstm_input_size, args.num_dong)
         self.all_cat_act = nn.ReLU()
-        self.all_cat_output_fc = nn.Linear(args.num_dong*3, args.num_dong)
+        self.all_cat_output_fc = nn.Linear(args.num_dong*3, self.num_dong*2)
         
-    def forward(self, x):
+    def forward(self, x, min_max_values, task):
         # data decomposition
         '''
         len(data[0]) = 3 : user / infect / temp
@@ -81,11 +85,11 @@ class GC_TG_LSTM(nn.Module):
         
         user_graph_list, infect_graph_list, temp_sig_list = x[0], x[1], x[2].squeeze(0) # [168, [ [node], [edge_list], [edge_weight] ] ]
         
-        self.lstm_input_tensor = torch.zeros([1, 24*self.lstm_sequence_length, self.lstm_input_size]).to(self.device)
+        self.lstm_input_tensor = torch.zeros([1, 48*self.lstm_sequence_length, self.lstm_input_size]).to(self.device)
         
-        for i in range(24*self.lstm_sequence_length):
+        for i in range(48*self.lstm_sequence_length):
             
-            graph_user = self.graph_conv_user1(user_graph_list[i][0].reshape([142,-1]).type(torch.FloatTensor).to(self.device), 
+            graph_user = self.graph_conv_user1(user_graph_list[i][0].reshape([self.num_dong,-1]).type(torch.FloatTensor).to(self.device), 
                                               user_graph_list[i][1].squeeze(0).to(self.device), user_graph_list[i][2].squeeze(0).to(self.device))
             graph_user = self.graph_act1_user1(graph_user)
             graph_user = self.graph_fc_user(graph_user.view(-1).type(torch.FloatTensor).to(self.device))
@@ -93,7 +97,7 @@ class GC_TG_LSTM(nn.Module):
 #             print('graph_user : ', graph_user)
 #             print(self.graph_fc_user.bias.data)
     
-            graph_infect = self.graph_conv_infect1(infect_graph_list[i][0].reshape([142,-1]).type(torch.FloatTensor).to(self.device), 
+            graph_infect = self.graph_conv_infect1(infect_graph_list[i][0].reshape([self.num_dong,-1]).type(torch.FloatTensor).to(self.device), 
                                                   infect_graph_list[i][1].squeeze(0).to(self.device), infect_graph_list[i][2].squeeze(0).to(self.device))
             graph_infect = self.graph_act1_infect1(graph_infect)
             graph_infect = self.graph_fc_infect(graph_infect.view(-1).type(torch.FloatTensor).to(self.device))
@@ -120,8 +124,8 @@ class GC_TG_LSTM(nn.Module):
 #             print('lstm_input : ',lstm_input)
             self.lstm_input_tensor[0][i] = lstm_input
        
-        h_0 = Variable(torch.randn([self.lstm_num_layers, 24*self.lstm_sequence_length, self.lstm_hidden_size], requires_grad=True)).to(self.device)
-        c_0 = Variable(torch.randn([self.lstm_num_layers, 24*self.lstm_sequence_length, self.lstm_hidden_size], requires_grad=True)).to(self.device)
+        h_0 = Variable(torch.randn([self.lstm_num_layers, 48*self.lstm_sequence_length, self.lstm_hidden_size], requires_grad=True)).to(self.device)
+        c_0 = Variable(torch.randn([self.lstm_num_layers, 48*self.lstm_sequence_length, self.lstm_hidden_size], requires_grad=True)).to(self.device)
         lstm_output, (h_out, c_out) = self.lstm(self.lstm_input_tensor, (h_0, c_0))
         # print('lstm_output : ', lstm_output)
         
@@ -146,5 +150,13 @@ class GC_TG_LSTM(nn.Module):
             lstm_input_all = self.all_cat_act(lstm_input_all)
             
             output = self.all_cat_output_fc(torch.cat((lstm_output, lstm_input_all, temp_sig_all)))
+            
+        user_pred, infect_pred = output[:self.num_dong], output[self.num_dong:]
         
-        return output
+        if task == 'real_feb':
+            user_pred = denormalize(user_pred, min_max_values[0]+min_max_values[2], min_max_values[1]+min_max_values[3])
+            infect_pred = denormalize(infect_pred, min_max_values[4]+min_max_values[6], min_max_values[5]+min_max_values[7])
+        elif task == 'fake_full':
+            user_pred = denormalize(user_pred, min_max_values[8]+min_max_values[10], min_max_values[9]+min_max_values[11])
+            infect_pred = denormalize(infect_pred, min_max_values[12]+min_max_values[14], min_max_values[13]+min_max_values[15])
+        return user_pred, infect_pred
